a6d253df6ddc76a7662c53293212c54cfa28b559
atomemeteore <alexisnguyen97@yahoo.fr>
2024-12-22 23:25:49 +0100

    Update Extraction_noms.py
    Modified script

--- Extraction_noms.py
+++ Extraction_noms.py
@@ -1,9 +1,18 @@
+# Liste des mots-clés à rechercher
+keywords = [
+    "Rotamase", "cell cycle", "cell division", 
+    "macromolecule biosynthetic process", "cellular macromolecule biosynthetic process", 
+    "cellular macromolecule metabolic process", "macromolecule metabolic process", 
+    "Oxidoreductase", "Antibioticbiosynthesis"
+]
 
+# Chemin des fichiers d'entrée et de sortie
+input_file_path = "/home/nguyeho3/Documents/TP_Naplas/Matrice_finale.txt"
+output_file_path = "/home/nguyeho3/Documents/TP_Naplas/output.txt"
 
-liste = ["Rotamase","cell cycle", "cell division", "macromolecule biosynthetic process", "cellular macromolecule biosynthetic process", "cellular macromolecule metabolic process","macromolecule metabolic process", "Oxidoreductase","Antibioticbiosynthesis"]
-with open ("G:\Mon Drive\CCB4\Analyse de données en sciences omiques\TP_NAPLAS\Results Stats sur Log\Matrice_finale.txt","r") as file :
-    with open("G:\Mon Drive\CCB4\Analyse de données en sciences omiques\TP_NAPLAS\Results Stats sur Log\output.txt","w") as outputFile:
-        for line in file and i in liste:
-            if i in line : 
-                outputFile.write(line)
-
+# Lecture et écriture des fichiers
+with open(input_file_path, "r", encoding="utf-8") as file, open(output_file_path, "w", encoding="utf-8") as output_file:
+    for line in file:
+        # Vérifie si un des mots-clés est présent dans la ligne
+        if any(keyword in line for keyword in keywords):
+            output_file.write(line)
